eed4aeb462e3a3b9b02a6640e4b90adb1d67159e
John Suico <johncdsuico@gmail.com>
2022-12-07 18:17:29 -0800

    Progress with feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,61 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+
+import { Sidebar, Videos } from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const Feed = () => {
+
+  const [selectedCategory, setSelectedCategory] = useState('New');
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+      .then ((data) => {
+        setVideos(data.items);
+      })
+  }, [selectedCategory])
+
   return (
-    <div>Feed</div>
+    <Stack
+      sx={{
+        flexDirection: { sx: "column", md: "row" }
+      }}
+    >
+      <Box
+        sx={{
+          height: { sx: "auto", md: "93vh" },
+          borderRight: "1px solid #3D3D3D",
+          px: { sx: 0, md: 2 }
+        }}
+      >
+        <Sidebar 
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
+
+        <Typography className="copyright" variant="body2"
+          sx={{
+            mt: 1.5,
+            color: "white"
+          }}
+        >
+          Suico Projects
+        </Typography>
+      </Box>
+
+      <Box p={2} sx={{
+        overflowY: 'auto',
+        height: '90vh'
+      }}>
+        <Typography variant="h4" fontWeight="bold" mb={2} color='white'>
+          {selectedCategory} <span style={{ color: '#F31503' }}>videos</span>
+        </Typography>
+
+        <Videos videos={videos}/>
+
+      </Box>
+    </Stack>
   )
 }
 
